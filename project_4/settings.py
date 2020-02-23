e56fb5a6157cd54689070615780e1e461967bdc2
Rafal Kruszewski <kruszewskirafael@gmail.com>
2020-02-23 14:56:07 +0000

    imported django_heroku

--- project_4/settings.py
+++ project_4/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 # import env
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -168,3 +169,5 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+
+django_heroku.settings(locals())
