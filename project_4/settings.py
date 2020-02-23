e59e54ab00122a17df6499a6eb775b3e90d40880
Rafal Kruszewski <kruszewskirafael@gmail.com>
2020-02-23 15:38:59 +0000

    test debug true, travis uncomment migrating commands

--- project_4/settings.py
+++ project_4/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['code-institute-project-4.herokuapp.com']
 
