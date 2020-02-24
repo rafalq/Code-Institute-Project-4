1ebf515e59be00bdf27ece80a0ee38e54dae053d
Rafal Kruszewski <kruszewskirafael@gmail.com>
2020-02-24 09:55:57 +0000

    added BASE_DIR staticfiles

--- project_4/settings.py
+++ project_4/settings.py
@@ -149,7 +149,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # STATICFILES_LOCATION = 'static/'
 # STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIAFILES_LOCATION = 'media'
