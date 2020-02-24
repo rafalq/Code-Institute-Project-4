281e76c413fa281751e4fc7b458c362372d63324
Rafal Kruszewski <kruszewskirafael@gmail.com>
2020-02-24 17:17:53 +0000

    changed DEBUG

--- project_4/settings.py
+++ project_4/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-#(os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 
 ALLOWED_HOSTS = ['code-institute-project-4.herokuapp.com']
