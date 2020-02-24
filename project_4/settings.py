9f5d7ace3e2e407e862c78e99cb60456d9bb8de8
Rafal Kruszewski <kruszewskirafael@gmail.com>
2020-02-24 12:20:58 +0000

    uncommented DATABASES dict

--- project_4/settings.py
+++ project_4/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'project_4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
